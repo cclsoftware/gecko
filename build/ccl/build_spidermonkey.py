6edf1ae5ad1673b5fa1e464d42017616a7a761ec
Thomas Kluge <tkluge@fender.com>
2025-02-10 12:18:32 +0100

    fix path

--- build/ccl/build_spidermonkey.py
+++ build/ccl/build_spidermonkey.py
@@ -175,7 +175,7 @@ elif platform == 'ios-sim' :
     build_one ('ios', 'x86_64')
 
     subprocess.run (['/usr/bin/lipo', '-create', '-output', 'libjs_static.a', 'obj-ios-x86_64/js/src/build/libjs_static.a', 'obj-ios-aarch64/js/src/build/libjs_static.a'])
-    subprocess.run (['/usr/bin/lipo', '-create', '-output', 'libjsrust.a', 'obj-ios-x86_64/x86_64-apple-ios/' + folder + '/libjsrust.a', 'obj-ios-aarch64/aarch64-apple-ios-sim/' + folder + '/libjsrust.a'])
+    subprocess.run (['/usr/bin/lipo', '-create', '-output', 'libjsrust.a', 'obj-ios-x86_64/x86_64-apple-ios/' + folder + '/libjsrust.a', 'obj-ios-sim-aarch64/aarch64-apple-ios-sim/' + folder + '/libjsrust.a'])
 
     with zipfile.ZipFile (basedir + '/spidermonkey-' + version + '.ios-sim.zip', mode = 'w') as buildproducts:
         os.chdir ('obj-' + platform + '-' + 'aarch64')
